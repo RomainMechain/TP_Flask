a076423cf9b00eeb2d802169a778369cd33088d8
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-09-08 12:26:26 +0200

    ajout d'un template

--- tuto/views.py
+++ tuto/views.py
@@ -1,5 +1,9 @@
 from .app import app
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "<h1>Hello world</h1>"
+    return render_template(
+        "home.html",
+        title="Hello world!",
+        names=["Pierre","Paul","Corinne"])
