f98dea55324a50ff23364b8d08daf0f86f9b00b8
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-10-13 12:16:28 +0200

    feature: ajout des donctions de représentation des classes Author et Book

--- tuto/models.py
+++ tuto/models.py
@@ -2,6 +2,8 @@ from .app import db
 class Author (db.Model ):
     id = db.Column(db.Integer , primary_key =True)
     name = db.Column (db.String (100))
+    def __repr__ (self ):
+        return "<Author(%d)%s>" % (self.id , self.name)
 class Book(db.Model ):
     id = db.Column(db.Integer , primary_key =True)
     price = db.Column(db.Float)
@@ -11,6 +13,8 @@ class Book(db.Model ):
     author_id = db.Column(db.Integer,db.ForeignKey("author.id"))
     author = db. relationship ("Author",
         backref =db.backref ("books",lazy="dynamic"))
+    def __repr__ (self ):
+        return "<Book(%d)%s>"%(self.id,self.title)
 
 
 def get_sample ():
