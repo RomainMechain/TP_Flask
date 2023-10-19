c9b85f4ed997ed4f6228cdd035c27cf86dc886e2
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-10-20 01:32:08 +0200

    Ajout de l'exercice 4 du TP4

--- tuto/views.py
+++ tuto/views.py
@@ -29,3 +29,20 @@ def edit_author(id):
     a = get_author(id)
     f = AuthorForm(id=a.id , name=a.name)
     return render_template("edit-author.html",author =a, form=f)
+
+from flask import url_for , redirect
+from .app import db
+from .models import Author
+
+@app.route("/save/author/", methods =("POST",))
+def save_author ():
+    a = None
+    f = AuthorForm ()
+    if f.validate_on_submit():
+        id = int(f.id.data)
+        a = get_author(id)
+        a.name = f.name.data
+        db.session.commit()
+        return redirect(url_for('one_author', id=a.id))
+    a = get_author(int(f.id.data))
+    return render_template("edit-author.html",author =a, form=f)
