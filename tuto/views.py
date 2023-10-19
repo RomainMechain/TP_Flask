c33f589c408c0e6da8e7beac1a66d51393e5dea0
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-10-19 17:46:13 +0200

    Fin de l'exercice 2

--- tuto/views.py
+++ tuto/views.py
@@ -16,3 +16,16 @@ def detail(id):
     print(book)
     return render_template(
     "detail.html",book=book)
+
+from flask_wtf import FlaskForm
+from wtforms import StringField , HiddenField
+from wtforms . validators import DataRequired
+
+class AuthorForm ( FlaskForm ):
+    id = HiddenField ('id')
+    name = StringField ('Nom', validators =[DataRequired()])
+@app.route("/edit/author/<int:id>")
+def edit_author(id):
+    a = get_author(id)
+    f = AuthorForm(id=a.id , name=a.name)
+    return render_template("edit-author.html",author =a, form=f)
