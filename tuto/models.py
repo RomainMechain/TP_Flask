b1924a3974f7d4e43c7bed6e8e5389038fba79d7
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2023-10-20 12:20:04 +0200

    refactoring

--- tuto/models.py
+++ tuto/models.py
@@ -1,9 +1,11 @@
 from .app import db
+
 class Author (db.Model ):
     id = db.Column(db.Integer , primary_key =True)
     name = db.Column (db.String (100))
     def __repr__ (self ):
         return "<Author(%d)%s>" % (self.id , self.name)
+
 class Book(db.Model ):
     id = db.Column(db.Integer , primary_key =True)
     price = db.Column(db.Float)
